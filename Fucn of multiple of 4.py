d7a73dad83e14e79a29e158f31d3c76dab34c9d9
Arpit Tiwari <arpittiwari4559@gmail.com>
2024-05-23 13:53:42 +0530

    Update and rename Fucn of multiple of 4.py to Fucn of multiple table of any no..py

--- Fucn of multiple of 4.py
+++
@@ -1,8 +0,0 @@
-def mul_4(a):
-   
-    b=4
-    for a in range (0,a+1):
-        c=b*a
-        print(c,end=" ")
-a=int(input("enter any number"))
-print(mul_4(a))
---
+++ Fucn of multiple table of any no..py
@@ -0,0 +1,9 @@
+def mul_():
+  #table of any no
+   
+    b=float(input("Enter the no.")
+    for a in range (0,10):
+            a+=1
+            c=b*a
+            print(b,"X",a,"=",c)
+mul_()
